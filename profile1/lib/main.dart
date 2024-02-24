6a62f77f2d58cd0e19f977f2d46ace8376a55925
Your Name <you@example.com>
2024-02-24 16:05:52 +0200

    adding

---
+++ profile1/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:profile1/profile.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(scaffoldBackgroundColor: Colors.black),
+      home: profile(),
+    );
+  }
+}
